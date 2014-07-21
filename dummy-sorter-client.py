4b074de1975c4a006b7ca52762b15c1b7ce509b4
Shea Polanskky <env.laser@gmail.com>
2014-07-21 13:25:14 -0700

    Added default_input(prompt, default, append_default_and_colon) to util.py Started using above

--- dummy-sorter-client.py
+++ dummy-sorter-client.py
@@ -9,8 +9,8 @@ sock = socket.socket(socket.AF_INET, #IP
 	socket.SOCK_DGRAM) #UDP
 
 while True:
-	packet = create_protocol_packet(int(input("Datatype [0x0001]: ") or "0x0001", 16),
-		input("Payload [Test]: ") or "Test", 
-		int(input("Protocol Header [0x09F9]: ") or "0x09F9", 16))
+	packet = create_protocol_packet(int(default_input("Datatype", "0x0001"), 16),
+		default_input("Payload", "Test"), 
+		int(input("Protocol Header", "0x09F9"), 16))
 	sock.sendto(packet, (UDP_IP, UDP_PORT))
 	print("Sent packet:", binascii.hexlify(packet))
--- util.py
+++ util.py
@@ -4,3 +4,6 @@ def bytes_to_short(bytes):
 	return struct.unpack(">h", bytes)[0]
 def short_to_bytes(short):
 	return struct.pack(">h", short)
+
+def default_input(prompt, default, append_default_and_colon = True):
+	return input(prompt + (" [%0]: " % default if append_default_and_colon else "")) or default
