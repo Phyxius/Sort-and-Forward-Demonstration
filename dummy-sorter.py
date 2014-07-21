164888388781ee2bd1e2afa7c1e55bfcc4c77866
Shea Polanskky <env.laser@gmail.com>
2014-07-21 11:17:45 -0700

    Dummy client and sorter now work! Added stuff to protocol.py

--- dummy-sorter.py
+++ dummy-sorter.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 import socket
 import binascii
-from protocol import read_protocol_packet
+from protocol import *
 
 UDP_IP = "" #equivalent to IPADDR_ANY
 UDP_PORT = 13337
@@ -13,10 +13,15 @@ sock.bind((UDP_IP, UDP_PORT))
 while True:
 	try:
 		source, datatype, payload = read_protocol_packet(sock)
-		print("Received packet:")
-		print("From:", source[0] + ":" + str(source[1]))
-		print("Datatype:", binascii.hexlify(datatype))
-		print("Payload:", binascii.hexlify(payload))
+		datatype = binascii.hexlify(bytes([ datatype ])).zfill(4)
+		if datatype in PROTOCOL_DATA_TYPES:
+			datatype = PROTOCOL_DATA_TYPES[datatype]
+		if datatype == "Text":
+			payload = payload.decode("utf-8")
+		else:
+			payload = binascii.hexlify(payload)
+		print("Received packet from", source[0] + ":" + str(source[1]))
+		print("Datatype:", datatype)
+		print("Payload:", payload)
 	except ConnectionError as e:
 		print(e)
-	
--- protocol.py
+++ protocol.py
@@ -4,15 +4,16 @@ import struct
 from util import *
 
 PROTOCOL_HEADER = 0x09F9
+PROTOCOL_DATA_TYPES = { b'0001': "Text", b'0002': "Audio", b'0003': "Video", b'0004': "Other" }
 
 def read_protocol_packet(sock):
-	header, addr = sock.recvfrom(2)
+	data, addr = sock.recvfrom(6, socket.MSG_PEEK)
 	print("received")
-	if bytes_to_short(header) != PROTOCOL_HEADER:
+	if bytes_to_short(data[:2]) != PROTOCOL_HEADER:
 		raise ConnectionError("Received invalid packet: Did not have proper protocol header (had '{}')".format(binascii.hexlify(header)))
-	datatype = sock.recvfrom(2)[0] #discard source address
-	length = bytes_to_short(sock.recvfrom(2)[0])
-	payload = sock.recvfrom(length)[0]
+	datatype = bytes_to_short(data[2:4])#discard source address
+	length = bytes_to_short(data[4:])
+	payload = sock.recv(6 + length)[6:]
 	return (addr, datatype, payload)
 
 def create_protocol_packet(datatype, payload, protocol_header = PROTOCOL_HEADER):
