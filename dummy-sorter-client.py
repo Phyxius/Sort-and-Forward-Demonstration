3ca00d1d3adceb732c005fb0773d5a76c43c9558
Shea Polanskky <env.laser@gmail.com>
2014-07-21 13:27:27 -0700

    Fixed previous commit

--- dummy-sorter-client.py
+++ dummy-sorter-client.py
@@ -1,6 +1,6 @@
 import socket
 import binascii
-from protocol import create_protocol_packet
+from protocol import *
 
 UDP_IP = "192.168.245.129"
 UDP_PORT = 13337
@@ -11,6 +11,6 @@ sock = socket.socket(socket.AF_INET, #IP
 while True:
 	packet = create_protocol_packet(int(default_input("Datatype", "0x0001"), 16),
 		default_input("Payload", "Test"), 
-		int(input("Protocol Header", "0x09F9"), 16))
+		int(default_input("Protocol Header", "0x09F9"), 16))
 	sock.sendto(packet, (UDP_IP, UDP_PORT))
 	print("Sent packet:", binascii.hexlify(packet))
--- util.py
+++ util.py
@@ -6,4 +6,4 @@ def short_to_bytes(short):
 	return struct.pack(">h", short)
 
 def default_input(prompt, default, append_default_and_colon = True):
-	return input(prompt + (" [%0]: " % default if append_default_and_colon else "")) or default
+	return input(prompt + (" [{}]: ".format(default) if append_default_and_colon else "")) or default
