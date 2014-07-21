9b45ebba7e9c3deb4d4edebaa9c811961be6b93e
Shea Polanskky <env.laser@gmail.com>
2014-07-21 12:50:54 -0700

    Switched raed_protocol_packet to use struct.unpack instead of a piecemeal approach Also moved the format string for the struct to a variable

--- protocol.py
+++ protocol.py
@@ -3,19 +3,20 @@ import binascii
 import struct
 from util import *
 
+PROTOCOL_HEADER_STRUCT_FORMAT = ">hhh"
 PROTOCOL_HEADER = 0x09F9
 PROTOCOL_DATA_TYPES = { b'0001': "Text", b'0002': "Audio", b'0003': "Video", b'0004': "Other" }
 
 def read_protocol_packet(sock):
 	data, addr = sock.recvfrom(6, socket.MSG_PEEK)
 	#print("received")
-	if bytes_to_short(data[:2]) != PROTOCOL_HEADER:
+	header, datatype, length = struct.unpack(">hhh", data)
+	if header != PROTOCOL_HEADER:
 		raise ConnectionError("Received invalid packet: Did not have proper protocol header (had '{}')".format(binascii.hexlify(header)))
-	datatype = bytes_to_short(data[2:4])#discard source address
-	length = bytes_to_short(data[4:])
 	payload = sock.recv(6 + length)[6:]
 	return (addr, datatype, payload)
 
 def create_protocol_packet(datatype, payload, protocol_header = PROTOCOL_HEADER):
 	payload_bytes = bytes(payload, "utf-8")
-	return struct.pack(">hhh", protocol_header, datatype, len(payload_bytes)) + payload_bytes
+	return struct.pack(PROTOCOL_HEADER_STRUCT_FORMAT, 
+		protocol_header, datatype, len(payload_bytes)) + payload_bytes
