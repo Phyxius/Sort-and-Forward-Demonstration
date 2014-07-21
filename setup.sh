26aa41eb844aa20af8ded5271db1087d9dc2feef
Shea Polanskky <env.laser@gmail.com>
2014-07-21 11:18:07 -0700

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get dist-upgrade -y
-sudo apt-get update 
-sudo apt-get dist-upgrade -y
 sudo apt-get remove thunderbird firefox gnumeric abiword pidgin gmusicbrowser parole simple-scan xchat -y
+sudo apt-get update
+sudo apt-get dist-upgrade -y
+sudo apt-get update
+sudo apt-get dist-upgrade -y
 sudo apt-get install vlc python3-pip -y
+ln -s "/mnt/hgfs/VM\ Shared\ Folder/" "~/Desktop/"
+sudo reboot
