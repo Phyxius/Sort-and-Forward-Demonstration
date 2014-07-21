86286da2a907b06f0ae53dc9aa6ac111e32aa043
Shea Polanskky <env.laser@gmail.com>
2014-07-21 13:48:09 -0700

    Commented a confusing line in protocol.py

--- protocol.py
+++ protocol.py
@@ -8,13 +8,13 @@ PROTOCOL_HEADER = 0x09F9
 PROTOCOL_DATA_TYPES = { b'0001': "Text", b'0002': "Audio", b'0003': "Video", b'0004': "Other" }
 
 def read_protocol_packet(sock):
-	data, addr = sock.recvfrom(6, socket.MSG_PEEK)
+	data, addr = sock.recvfrom(6, socket.MSG_PEEK) #read first part of packet without allocating a large buffer or discarding the rest
 	#print("received")
 	header, datatype, length = struct.unpack(PROTOCOL_HEADER_STRUCT_FORMAT, data)
 	if header != PROTOCOL_HEADER:
 		sock.recv(1) #discard current packet
 		raise ConnectionError("Received invalid packet: Did not have proper protocol header (had '0x%0.2X')" % header)
-	payload = sock.recv(6 + length)[6:]
+	payload = sock.recv(6 + length)[6:] #skip header
 	return (addr, datatype, payload)
 
 def create_protocol_packet(datatype, payload, protocol_header = PROTOCOL_HEADER):
