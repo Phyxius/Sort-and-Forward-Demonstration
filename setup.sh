5dd996c88d7abf9d85c97912f8f00a5b1f06a644
Shea Polanskky <env.laser@gmail.com>
2014-07-21 14:25:39 -0700

    Added -y to apt autoremove

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install vlc python3-pip -y
 sudo apt-get update
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 sudo apt-get autoclean
 ln -s "/mnt/hgfs/VM\ Shared\ Folder/" "~/Desktop/"
 sudo reboot
