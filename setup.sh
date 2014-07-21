6233c196493f303d3a0280fedc061a488fd18bb8
Shea Polanskky <env.laser@gmail.com>
2014-07-21 13:45:45 -0700

    Improved setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get remove thunderbird firefox gnumeric abiword pidgin gmusicbrowser parole simple-scan xchat -y
+sudo apt-get remove thunderbird firefox gnumeric abiword pidgin gmusicbrowser parole simple-scan transmission-common xchat  -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get install vlc python3-pip -y
+sudo apt-get update
+sudo apt-get autoremove
+sudo apt-get autoclean
 ln -s "/mnt/hgfs/VM\ Shared\ Folder/" "~/Desktop/"
 sudo reboot
