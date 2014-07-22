7f36245784e14c752465393636d9157b0f925de7
Shea Polanskky <env.laser@gmail.com>
2014-07-22 13:16:51 -0700

    Moved sorter.py to use fewer magic numbers

--- sorter.py
+++ sorter.py
@@ -2,7 +2,7 @@ import socket
 from protocol import *
 from util import *
 
-FORWARD_TABLE = { 1:"192.168.245.130", 2:"192.168.245.131", 3:"192.168.245.132", 4:"192.168.245.133" }
+FORWARD_TABLE = { "Text":"192.168.245.130", "Audio":"192.168.245.131", "Video":"192.168.245.132", "Other":"192.168.245.133" }
 
 UDP_IP = "" #equivalent to IPADDR_ANY
 UDP_PORT = 13337
@@ -17,9 +17,10 @@ client_sock = socket.socket(socket.AF_INET, #IP
 while True:
 	try:
 		source, datatype, payload = read_protocol_packet(sock)
+		datatype = PROTOCOL_DATA_TYPES[datatype] if datatype in PROTOCOL_DATA_TYPES else datatype
 		print("Received packet from {} with datatype {} and payload length {}".format(
-			source[0] + ":" + str(source[1]), PROTOCOL_DATA_TYPES[datatype] if datatype in PROTOCOL_DATA_TYPES else datatype, len(payload)))
-		destination = FORWARD_TABLE[datatype] if datatype in FORWARD_TABLE else FORWARD_TABLE[4]
+			source[0] + ":" + str(source[1]), datatype, len(payload)))
+		destination = FORWARD_TABLE[datatype] if datatype in FORWARD_TABLE else FORWARD_TABLE["Other"]
 		print("\tForwarded to", destination)
 		client_sock.sendto(payload, (destination, UDP_PORT))
 	except ConnectionError as e:
