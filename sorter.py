b70a5fb1586262fa5a3f0637c18271a0f2eedb3e
Shea Polanskky <env.laser@gmail.com>
2014-07-22 13:19:48 -0700

    More descriptive socket name in sorter.py

--- sorter.py
+++ sorter.py
@@ -7,16 +7,16 @@ FORWARD_TABLE = { "Text":"192.168.245.130", "Audio":"192.168.245.131", "Video":"
 UDP_IP = "" #equivalent to IPADDR_ANY
 UDP_PORT = 13337
 
-sock = socket.socket(socket.AF_INET, #IP
+server_sock = socket.socket(socket.AF_INET, #IP
 	socket.SOCK_DGRAM) #UDP
-sock.bind((UDP_IP, UDP_PORT))
+server_sock.bind((UDP_IP, UDP_PORT))
 
 client_sock = socket.socket(socket.AF_INET, #IP
 	socket.SOCK_DGRAM) #UDP
 
 while True:
 	try:
-		source, datatype, payload = read_protocol_packet(sock)
+		source, datatype, payload = read_protocol_packet(server_sock)
 		datatype = PROTOCOL_DATA_TYPES[datatype] if datatype in PROTOCOL_DATA_TYPES else datatype
 		print("Received packet from {} with datatype {} and payload length {}".format(
 			source[0] + ":" + str(source[1]), datatype, len(payload)))
