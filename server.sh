88948edcc1381961d85b22b5c7d33dba853367e0
Shea Polanskky <env.laser@gmail.com>
2014-07-29 09:54:14 -0700

    Demo automation scripts work now

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-xfce4-terminal --command="python3 forwarder.py" &
+x-terminal-emulator -e 'bash -c "python3 forwarder.py"'
 sleep 1
-xfce4-terminal --command="pv -L 500k Data/demodata/video.avi | nc -u localhost 13339" &
-xfce4-terminal --command="pv -L 80k Data/demodata/audio.mp3 | nc -u localhost 13338" &
-xfce4-terminal --command="pv -L 10 Data/demodata/text.txt | nc -u localhost 13337" &
-xfce4-terminal --command="nc -u localhost 13340" &
+x-terminal-emulator -e 'bash -c "pv -L 500k Data/demodata/video.avi | nc -u localhost 13339 "'
+x-terminal-emulator -e 'bash -c "pv -L 80k Data/demodata/audio.mp3 | nc -u localhost 13338 "'
+x-terminal-emulator -e 'bash -c "pv -L 10 Data/demodata/text.txt | nc -u localhost 13337 "'
+x-terminal-emulator -e 'bash -c "nc -u localhost 13340"'
--- sorter.sh
+++ sorter.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-xfce4-terminal --command="python3 sorter.py && pause"
+python3 sorter.py
