5046e38370297b81f7a23a022739ffb575d3ae84
Shea Polanskky <env.laser@gmail.com>
2014-07-31 09:45:28 -0700

    Added #!'s to files that needed them

--- receiver.sh
+++ receiver.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
 x-terminal-emulator -e 'bash -c "python3 nc.py 0x11 | pv | buffer -s 500k -m 30m | avplay -"' #'audio'
 x-terminal-emulator -e 'bash -c "python3 nc.py 0x22 | pv | buffer -s 100k -m 8m | avplay -"' #video
 x-terminal-emulator -e 'bash -c "python3 nc.py 0x33"' #text
--- sorter.py
+++ sorter.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python3
 import socket
 from protocol import *
 from util import *
