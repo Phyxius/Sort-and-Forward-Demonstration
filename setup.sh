ffbd31ecd633d51eaa6df13999ce5050404b044f
Shea Polanskky <env.laser@gmail.com>
2014-07-31 09:22:23 -0700

    Updated setup script to reflect changes in standard software

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 sudo apt-get update
-sudo apt-get remove thunderbird firefox gnumeric abiword pidgin gmusicbrowser parole simple-scan transmission-common xchat  -y
+sudo apt-get remove thunderbird firefox gnumeric abiword pidgin gmusicbrowser parole simple-scan transmission-common xchat -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt-get install vlc python3-pip -y
+sudo apt-get install vlc python3-pip wireshark pv libav-tools -y
 sudo apt-get update
 sudo apt-get autoremove -y
 sudo apt-get autoclean
